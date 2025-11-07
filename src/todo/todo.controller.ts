46b2ad367001997f9843c7d2b8e56957e70c1a1a
AbdulRehman247136 <su92-bssem-f23-094@superior.edu.pk>
2025-11-07 09:35:00 +0500

    the 2 working api

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { TodoService } from './todo.service';
 
 @Controller('todo')
-export class TodoController {}
+export class TodoController {
+  constructor(private readonly todoService: TodoService) {}
+
+  @Get()
+  getAllTodos() {
+    return this.todoService.getAllTodos();
+  }
+
+  @Get(':id')
+  getSingleTodo(@Param('id') id: string) {
+    return this.todoService.getSingleTodo(id);
+  }
+}
--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+  getAllTodos() {
+    return 'This is for all todos';
+  }
+
+  getSingleTodo(id: string) {
+    return 'This is for single todo id ' + id;
+  }
+
+  saveTodo(title:string, description: string) {
+    return(
+        'sa'
+    )
+  }
+}
